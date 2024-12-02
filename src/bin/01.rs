6fee2b534dc92ceb5ffdaba6746ccea4cdd40abf
Kai Yang <kai.yang@intradiem.com>
2024-12-02 16:59:12 -0500

    day1 solution.

---
+++ src/bin/01.rs
@@ -0,0 +1,87 @@
+use adv_code_2024::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "01";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        match read(reader) {
+            Result::Ok((mut left, mut right)) => {
+                left.sort();
+                right.sort();
+                let answer: usize = left
+                    .into_iter()
+                    .zip(right.into_iter())
+                    .map(|(l, r)| (l.abs_diff(r)))
+                    .sum();
+                Ok(answer)
+            }
+            Err(e) => return Err(e),
+        }
+    }
+
+    assert_eq!(11, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        match read(reader) {
+            Result::Ok((left, right)) => {
+                let answer: usize = left
+                    .into_iter()
+                    .map(|e| e * right.iter().filter(|&&ee| ee == e).count())
+                    .sum();
+                Ok(answer)
+            }
+            Err(e) => return Err(e),
+        }
+    }
+
+    assert_eq!(31, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
+
+fn read<R: BufRead>(reader: R) -> Result<(Vec<usize>, Vec<usize>)> {
+    let mut left: Vec<usize> = Vec::new();
+    let mut right: Vec<usize> = Vec::new();
+    for result in reader.lines() {
+        let line = result?;
+
+        let mut iter = line.split_whitespace();
+
+        left.push(iter.next().unwrap().parse::<usize>()?);
+        right.push(iter.next().unwrap().parse::<usize>()?);
+    }
+    Ok((left, right))
+}
